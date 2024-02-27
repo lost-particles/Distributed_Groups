3f7942d93c0db843062e8e3b428718fba8bc1aaa
subham <subhamkumardas852@gmail.com>
2024-02-27 14:20:36 -0500

    Update_3.4

--- distribution/local/groups.js
+++ distribution/local/groups.js
@@ -1,5 +1,6 @@
 id = require('../util/id.js');
 distribution = require('../../distribution');
+const path = require('path');
 
 const groups = {
   groupMapping: new Map(),
@@ -12,11 +13,11 @@ const groups = {
   },
   put: function(key, group, callback=console.log) {
     if (distribution[key]===undefined) {
-      distribution[key]={'comm': require('../all/comm.js')({gid: key}),
-        'status': require('../all/status.js')({gid: key}),
-        'groups': require('../all/groups')({gid: key}),
-        'routes': require('../all/routes.js')({gid: key}),
-        'gossip': require('../all/gossip.js')({gid: key})};
+      distribution[key]={'comm': require(path.join(__dirname, '../all/comm.js'))({gid: key}),
+        'status': require(path.join(__dirname, '../all/status.js') )({gid: key}),
+        'groups': require(path.join(__dirname, '../all/groups') )({gid: key}),
+        'routes': require(path.join(__dirname, '../all/routes.js') )({gid: key}),
+        'gossip': require(path.join(__dirname, '../all/gossip.js') )({gid: key})};
     }
     if (this.groupMapping.has(key)) {
       this.groupMapping.set(key, {...this.groupMapping.get(key), ...group});
