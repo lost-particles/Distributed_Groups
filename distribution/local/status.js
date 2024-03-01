e2ce580dcc34ae3ff46f12e85b5e4080fe1982e7
subham <subhamkumardas852@gmail.com>
2024-02-29 21:59:17 -0500

    Update_3.18

--- distribution/local/status.js
+++ distribution/local/status.js
@@ -51,13 +51,13 @@ status.spawn = function(config, cb=console.log) {
       cb(null, ...args);
     }));
   }
-  // const cp = childProcess.fork(path.join(__dirname, '../../distribution.js'),
-  //     [serialization.serialize(config)]);
+  const cp = childProcess.fork(path.join(__dirname, '../../distribution.js'),
+      [serialization.serialize(config)]);
   console.log('Path generated here is : '+
     path.join(__dirname, '../distribution.js'));
-  const cp = childProcess.spawn('node',
-      ['../../distribution.js', '--config', serialization.serialize(config)],
-      {detached: true, stdio: 'inherit'});
+  // const cp = childProcess.spawn('node',
+  //     ['../../distribution.js', '--config', serialization.serialize(config)],
+  //     {detached: true, stdio: 'inherit'});
   cp.on('error', (error) => {
     console.error('Error in child process:', error);
   });
