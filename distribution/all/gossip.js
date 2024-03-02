947a1ee9bdf90997342a79d0a1bb37fdba346f4b
subham <subhamkumardas852@gmail.com>
2024-03-01 20:51:01 -0500

    Update_4.5

--- distribution/all/gossip.js
+++ distribution/all/gossip.js
@@ -1,9 +1,9 @@
-
-
-let gossip = (config) => {
-  let context = {};
-  context.gid = config.gid || 'all'; // contains a property named gid
-  return {send: () => {/** uses context **/}};
-};
-
-module.exports = gossip;
+//
+//
+// let gossip = (config) => {
+//   let context = {};
+//   context.gid = config.gid || 'all'; // contains a property named gid
+//   return {send: () => {/** uses context **/}};
+// };
+//
+// module.exports = gossip;
